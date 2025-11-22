a98c1cadfc4d54aaa6db35c7c78167375b65a519
satishreddy955 <168215588+satishreddy955@users.noreply.github.com>
2025-11-22 09:58:21 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:4000/api'   // IMPORTANT
+  baseURL: 'https://inventory-management-1-p0zc.onrender.com/api'  
 });
 
 export default API;
